1f466c4a38fc4a49f3412fb147a0feabd25e6e4e
jp-quintana <juanquintana1996@gmail.com>
2026-04-03 21:49:25 +0000

    feat: add sort for auctions

--- server/src/modules/auctions/auctions.service.ts
+++ server/src/modules/auctions/auctions.service.ts
@@ -2,9 +2,15 @@ import { Inject, Injectable } from '@nestjs/common';
 import { AuctionsQueryDto } from './dtos';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as auctionsSchema from './schemas';
-import { and, eq, isNull } from 'drizzle-orm';
+import { and, eq, gte, isNull } from 'drizzle-orm';
 import { AuctionsQueryRelations } from 'src/common/types';
-import { DATABASE_CONNECTION, DEFAULT_PAGE_SIZE } from 'src/common/constants';
+import {
+  AUCTION_SORT_CREATED_AT_ASC,
+  AUCTION_SORT_CREATED_AT_DESC,
+  AUCTION_SORT_ENDING_SOONEST,
+  DATABASE_CONNECTION,
+  DEFAULT_PAGE_SIZE,
+} from 'src/common/constants';
 
 @Injectable()
 export class AuctionsService {
@@ -19,7 +25,6 @@ export class AuctionsService {
   ) {
     const page = auctionsQueryDto.page || 1;
     const pageSize = auctionsQueryDto.pageSize || DEFAULT_PAGE_SIZE;
-    const sort = auctionsQueryDto.sort || '';
 
     return await this.db.query.auctions.findMany({
       where: and(
@@ -27,9 +32,21 @@ export class AuctionsService {
         auctionsQueryDto.status
           ? eq(auctionsSchema.auctions.status, auctionsQueryDto.status)
           : undefined,
+        auctionsQueryDto.sort === AUCTION_SORT_ENDING_SOONEST
+          ? gte(auctionsSchema.auctions.endTime, new Date())
+          : undefined,
       ),
       orderBy: (auctions, { asc, desc }) => {
-        return desc(auctions.createdAt);
+        switch (auctionsQueryDto.sort) {
+          case AUCTION_SORT_CREATED_AT_ASC:
+            return asc(auctions.createdAt);
+          case AUCTION_SORT_CREATED_AT_DESC:
+            return desc(auctions.createdAt);
+          case AUCTION_SORT_ENDING_SOONEST:
+            return asc(auctions.endTime);
+          default:
+            return desc(auctions.createdAt);
+        }
       },
       limit: pageSize,
       offset: (page - 1) * pageSize,
